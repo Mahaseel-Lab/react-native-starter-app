14f73e3011d2e5c961385f1b7592aa1dd07b5b3d
Mina Sameh <mina.s.wadie@gmail.com>
2023-01-24 10:26:19 +0200

    Misc[SplashScreen]: Added callback while splash loads

--- src/components/splashscreen.tsx
+++ src/components/splashscreen.tsx
@@ -1,6 +1,8 @@
-import { AnimatedLottieViewProps } from 'lottie-react-native'
-import LottieView from 'lottie-react-native'
+import { useFocusEffect } from '@react-navigation/native'
+import LottieView, { AnimatedLottieViewProps } from 'lottie-react-native'
+import { useCallback } from 'react'
 import { Dimensions, StyleSheet } from 'react-native'
+import { useAuthContext } from '../common/context/authContext'
 
 interface Props {
   setLoaded: (state: boolean) => void
@@ -9,8 +11,15 @@ interface Props {
 
 export const SplashScreen = ({ setLoaded, resizeMode = 'contain' }: Props) => {
   const { height, width } = Dimensions.get('window')
+  const { getToken } = useAuthContext()
   // Note: You can set resizeMode prop to resize the view.
 
+  useFocusEffect(
+    useCallback(() => {
+      // Ex: Prepare Token from storage
+      getToken()
+    }, [getToken])
+  )
   return (
     <LottieView
       source={require('../assets/splash-screen.json')}
