19fd38cf3abd4fac49de9fea9ee624cae486e8cd
Mina Sameh <mina.s.wadie@gmail.com>
2023-01-24 10:15:08 +0200

    Fixed Splash screen taking wrong size.

--- src/components/splashscreen.tsx
+++ src/components/splashscreen.tsx
@@ -1,27 +1,33 @@
+import { AnimatedLottieViewProps } from 'lottie-react-native'
 import LottieView from 'lottie-react-native'
-import { StyleSheet } from 'react-native'
+import { Dimensions, StyleSheet } from 'react-native'
 
 interface Props {
   setLoaded: (state: boolean) => void
+  resizeMode: AnimatedLottieViewProps['resizeMode']
 }
 
-export const SplashScreen = ({ setLoaded }: Props) => (
-  <LottieView
-    style={styles.splash}
-    source={require('../assets/splash-screen.json')}
-    autoPlay
-    loop={false}
-    resizeMode="center"
-    onAnimationFinish={() => {
-      setLoaded(true)
-    }}
-  />
-)
+export const SplashScreen = ({ setLoaded, resizeMode = 'contain' }: Props) => {
+  const { height, width } = Dimensions.get('window')
+  // Note: You can set resizeMode prop to resize the view.
+
+  return (
+    <LottieView
+      source={require('../assets/splash-screen.json')}
+      style={[styles.splash, { aspectRatio: width / height }]}
+      autoPlay
+      loop={true}
+      resizeMode={resizeMode}
+      onAnimationFinish={() => {
+        setLoaded(false)
+      }}
+    />
+  )
+}
 
 const styles = StyleSheet.create({
   splash: {
     flex: 1,
-    padding: 15,
     alignItems: 'center',
     margin: 0
   }
